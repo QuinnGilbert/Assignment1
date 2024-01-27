3aa76683e5460e0a0c4933891829c44eeba9820d
QuinnG <quinngilbert12and3@gmail.com>
2024-01-26 21:37:07 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Assignment1
+
+To run:
+g++ -O3 findPrimes.cpp && ./a.out > primes.txt
+
+# Proof of Correctness
+My program simply splits the search space into 8 equal parts and runs each part on a different thread. Each prime is tested in O(sqrt(n)) by a simple prime check, however as a constant time optimization, My code makes use of wheel factorization https://en.wikipedia.org/wiki/Wheel_factorization with the first two primes.
+
+# Efficiency
+There exists faster prime finding algorithms than the one used here, such as Miller-Rabin or the Sieve of Eratosthenes, however the Miller-Rabin is a probabilistic prime finder, and I opted for a deterministic approach, and the Sieve of Eratosthenes does not parallelize well without thread pooling (which I assumed to be outside the scope of this assignment). The O(Nsqrt(N)) prime check is still much faster than the naive O(N^2) prime check. The work is not split mostly equally among the 8 threads
+
+# Experimental Evaluation
+Decreasing the number of threads significantly increases the runtime, almost perfectly inversely proportionally so that halving the number of threads almost doubles the runtime.
+
---
+++ findPrimes.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPrime(int x) {
+    if(x <= 1) return false;
+    if(x <= 3) return true;
+    if (x % 2 == 0 || x % 3 == 0) return false;
+
+    for (int i = 5; i *i <= x; i += 6) {
+        if (x % i == 0 || x % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int numThreads = 1;
+
+    int N = 100'000'001;
+    vector<bool> prime(N);
+
+    vector<std::thread> threads(numThreads);
+    for(int thread=0; thread<numThreads; thread++) {
+        int start = N / numThreads * (thread);
+        int end = N / numThreads * (thread+1);
+        if(thread == numThreads-1) end = N;
+
+        auto testPrimes = [&](int start, int end) {
+            for(int i=start; i<end; i++) {
+                if(isPrime(i)) {
+                    prime[i] = true;
+                }
+            }
+        };
+
+        threads[thread] = std::thread(testPrimes, start, end);
+    }
+    
+    for(int i=0;i<numThreads;i++) {
+        threads[i].join();
+    }
+
+    int count = 0;
+    long long sum = 0;
+    vector<int> maxPrimes;
+    for(int i=N-1;i>=0;i--) {
+        if(prime[i]) {
+            if(maxPrimes.size() < 10) maxPrimes.push_back(i);
+            count++;
+            sum += i;
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    cout<<duration.count()/1000.0<<"s "<<count<<" "<<sum<<" "<<endl;
+    for(int i=maxPrimes.size()-1;i>=0;i--) {
+        cout<<maxPrimes[i]<<endl;
+    }
+
+}
